dcd0781a0c437c9db4c050ca1c69316621ddc112
Saidur Rohman <saidhstu@gmail.com>
2023-08-03 08:29:05 +0600

    adding file to git

---
+++ terraform_code/V2-EC2.tf
@@ -0,0 +1,41 @@
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+  ami = "ami-0f34c5ae932e6f0e4"
+  instance_type = "t2.micro"
+  key_name = "dpp-terraform"
+  tags = {
+    Name = "tarraform-instance"
+  }
+  security_groups = ["demo-sg"]
+
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH access"
+
+  ingress {
+    description      = "SSH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
+}
